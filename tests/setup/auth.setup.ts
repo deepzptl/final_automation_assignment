73235522f783ebe64d75a61b197f4520a4c4386c
deepzptl <deep.patel@simformsolutions.com>
2026-06-23 17:10:23 +0530

    Add tags to Authenticate and Save State test

--- tests/setup/auth.setup.ts
+++ tests/setup/auth.setup.ts
@@ -2,7 +2,7 @@ import { test } from '../../fixtures/customFixture';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-test('Authenticate and Save State', async ({ loginPage }) => {
+test('Authenticate and Save State @smoke @regression @all', async ({ loginPage }) => {
   await loginPage.login(process.env.USER_EMAIL!, process.env.USER_PASSWORD!);
   await loginPage.saveCurrentState('login-state.json');
 });
